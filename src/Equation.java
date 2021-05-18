1f5f68200e9e60ddae69d4d27a582ef989dac243
Kian Kasad <kian@kasad.com>
2021-05-17 19:27:35 -0700

    graph: add javadoc comments to Equation class

--- src/Equation.java
+++ src/Equation.java
@@ -1,14 +1,29 @@
 import net.objecthunter.exp4j.*;
 
+/**
+ * Represents an equation
+ * @author Kian Kasad
+ * @version 2021-05-17
+ */
 public class Equation {
 	private Expression e;
 
+	/**
+	 * Create an equation
+	 * @param equationString the equation typed as a string
+	 * TODO: add docs about proper syntax
+	 */
 	public Equation(String equationString) {
 		/* strip leading 'y = ' */
 		equationString = equationString.replaceAll("^\\s*y\\s*=\\s*", "");
 		e = new ExpressionBuilder(equationString).variable("x").build();
 	}
 
+	/**
+	 * Find the value of 'y' at the given value for 'x'
+	 * @param x the value of 'x'
+	 * @return the value of 'y' at the given 'x'
+	 */
 	public double evaluate(double x) {
 		return e.setVariable("x", x).evaluate();
 	}
